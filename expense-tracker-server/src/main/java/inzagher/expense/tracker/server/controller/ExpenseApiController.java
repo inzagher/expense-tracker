6919f138b91cc2bc84ea1b6ed675b78182238999
inzagher <IvanovDV87@yandex.ru>
2020-10-07 20:18:28 +0300

    Expense controller.

--- expense-tracker-server/src/main/java/inzagher/expense/tracker/server/controller/ExpenseApiController.java
+++ expense-tracker-server/src/main/java/inzagher/expense/tracker/server/controller/ExpenseApiController.java
@@ -3,7 +3,11 @@ package inzagher.expense.tracker.server.controller;
 import inzagher.expense.tracker.server.dto.ExpenseDTO;
 import inzagher.expense.tracker.server.service.ExpenseService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("expenses")
@@ -15,8 +19,18 @@ public class ExpenseApiController {
         this.expenseService = expenseService;
     }
     
-    @GetMapping("get")
-    public ExpenseDTO get(String id) {
+    @GetMapping("/{id}")
+    public ExpenseDTO read(@PathVariable String id) {
         return expenseService.getExpense(id).orElse(null);
+    }
+
+    @PostMapping("/")
+    public void save(@RequestBody ExpenseDTO expense) {
+        expenseService.storeExpense(expense);
+    }
+    
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable String id) {
+        expenseService.deleteExpense(id);
     }
 }
