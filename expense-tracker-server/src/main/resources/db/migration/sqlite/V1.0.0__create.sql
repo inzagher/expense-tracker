5ecdc57bd6a8f9da88b78e64c87246ed97596434
Inzagher <IvanovDV87@yandex.ru>
2020-11-12 11:16:20 +0300

    SQLite guid type in creation query.

--- expense-tracker-server/src/main/resources/db/migration/sqlite/V1.0.0__create.sql
+++ expense-tracker-server/src/main/resources/db/migration/sqlite/V1.0.0__create.sql
@@ -1,4 +1,4 @@
-create table backups (id blob not null, categories integer, expenses integer, persons integer, time datetime, primary key (id));
-create table persons (id blob not null, name varchar(255), primary key (id));
-create table categories (id blob not null, color_blue tinyint, color_green tinyint, color_red tinyint, description varchar(255), name varchar(255), primary key (id));
-create table expense (id blob not null, amount float(19), date datetime, description varchar(255), category_id blob, person_id blob, primary key (id));
+create table backups (id GUID not null, categories integer, expenses integer, persons integer, time datetime, primary key (id));
+create table persons (id GUID not null, name varchar(255), primary key (id));
+create table categories (id GUID not null, color_blue tinyint, color_green tinyint, color_red tinyint, description varchar(255), name varchar(255), primary key (id));
+create table expense (id GUID not null, amount float(19), date datetime, description varchar(255), category_id GUID, person_id GUID, primary key (id));
