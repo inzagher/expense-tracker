dbb1cfa828c163d719b8e03c145e33599aa7acce
inzagher <IvanovDV87@yandex.ru>
2020-12-16 19:27:06 +0300

    Reorganized application configuration.

--- expense-tracker-server/src/main/java/inzagher/expense/tracker/server/ServiceRunner.java
+++ expense-tracker-server/src/main/java/inzagher/expense/tracker/server/ServiceRunner.java
@@ -1,13 +1,41 @@
 package inzagher.expense.tracker.server;
 
+import inzagher.expense.tracker.server.core.BackupDataOutbox;
+import inzagher.expense.tracker.server.impl.FileBackupDataOutbox;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableScheduling
 @SpringBootApplication
-public class ServiceRunner {
+public class ServiceRunner implements WebMvcConfigurer {
+    @Value("${spring.resources.static-locations}")
+    private String resourceLocations;
+    @Value("${backup.directory}")
+    private String backupDirectoryPath;
+
     public static void main(String[] args) {
         SpringApplication.run(ServiceRunner.class, args);
+    }
+
+    @Bean
+    public BackupDataOutbox fileBackupDataOutbox() {
+        return new FileBackupDataOutbox(backupDirectoryPath);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler(
+                "/**/*.css", "/**/*.html",
+                "/**/*.js",  "/**/*.jsx",
+                "/**/*.png", "/**/*.ttf",
+                "/**/*.woff", "/**/*.woff2",
+                "/**/*.ico")
+                .setCachePeriod(0)
+                .addResourceLocations(resourceLocations);
     }
 }
