49933b613d97add1a05486f217942b2cb06633b0
inzagher <IvanovDV87@yandex.ru>
2021-02-13 00:47:09 +0300

    Person dialog component.

--- expense-tracker-web-client/src/app/material.module.ts
+++ expense-tracker-web-client/src/app/material.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
     exports: [
+        FormsModule,
         MatButtonModule,
+        MatInputModule,
+        MatFormFieldModule,
         MatMenuModule,
         MatIconModule,
         MatTableModule,
--- expense-tracker-web-client/src/app/view/dialog/person-editor-dialog/person-editor-dialog.component.ts
+++ expense-tracker-web-client/src/app/view/dialog/person-editor-dialog/person-editor-dialog.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+import { PersonService } from './../../../service/person.service';
+import { Person } from './../../../model/person';
 
 @Component({
    selector: 'app-person-editor-dialog',
@@ -6,6 +10,42 @@ import { Component, OnInit } from '@angular/core';
    styleUrls: ['./person-editor-dialog.component.scss']
 })
 export class PersonEditorDialogComponent implements OnInit {
-    constructor() { }
-    ngOnInit(): void { }
+    public model: Person | null = null;
+    public error: string | null = null;
+
+    constructor(
+        @Inject(MAT_DIALOG_DATA) private id: string | null,
+        private dialogRef: MatDialogRef<PersonEditorDialogComponent, boolean>,
+        private personService: PersonService
+    ) { }
+
+    ngOnInit(): void {
+        if (this.id == null) {
+            this.model = new Person();
+            return;
+        }
+
+        this.personService.getById(this.id).subscribe(
+            (person) => { this.model = person; },
+            (error) => { this.error = 'Failed to load data.'; }
+        );
+    }
+
+    isValid(): boolean {
+        return !!this.model && !!this.model.name && this.model.name.length > 0;
+    }
+
+    submit(): void {
+        if (this.model) {
+            this.error = null;
+            this.personService.save(this.model).subscribe(
+                () => { this.dialogRef.close(true); },
+                (e) => { this.error = 'Failed to store data.'; }
+            );
+        }
+    }
+
+    cancel(): void {
+        this.dialogRef.close(false);
+    }
 }
