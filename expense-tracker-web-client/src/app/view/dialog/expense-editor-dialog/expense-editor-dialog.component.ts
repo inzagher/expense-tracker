f0b811fa654a53d0fb82caaad0bdd6d59eb29159
inzagher <IvanovDV87@yandex.ru>
2021-03-01 00:41:53 +0300

    Expense editor dialog functionality.

--- expense-tracker-web-client/src/app/view/dialog/expense-editor-dialog/expense-editor-dialog.component.ts
+++ expense-tracker-web-client/src/app/view/dialog/expense-editor-dialog/expense-editor-dialog.component.ts
@@ -1,15 +1,72 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { concat, iif, Observable, of } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { Category } from 'src/app/model/category';
+
+import { Expense } from 'src/app/model/expense';
+import { Person } from 'src/app/model/person';
+import { CategoryService } from 'src/app/service/category.service';
+import { ExpenseService } from 'src/app/service/expense.service';
+import { PersonService } from 'src/app/service/person.service';
+import { RxUtils } from 'src/app/util/rx-utils';
 
 @Component({
-  selector: 'app-expense-editor-dialog',
-  templateUrl: './expense-editor-dialog.component.html',
-  styleUrls: ['./expense-editor-dialog.component.scss']
+    selector: 'app-expense-editor-dialog',
+    templateUrl: './expense-editor-dialog.component.html',
+    styleUrls: ['./expense-editor-dialog.component.scss']
 })
 export class ExpenseEditorDialogComponent implements OnInit {
+    public categories: Category[] | null = null;
+    public persons: Person[] | null = null;
+    public model: Expense | null = null;
+    public error: string | null = null;
 
-  constructor() { }
+    constructor(
+        @Inject(MAT_DIALOG_DATA) private id: number | null,
+        private dialogRef: MatDialogRef<ExpenseEditorDialogComponent, boolean>,
+        private expenseService: ExpenseService,
+        private categoryService: CategoryService,
+        private personService: PersonService
+    ) { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        let persons$ = this.personService.list().pipe(
+            tap(list => this.persons = list)
+        );
+        let categories$ = this.categoryService.list().pipe(
+            tap(list => this.categories = list)
+        );
+        let expense$ = this.loadExpense().pipe(
+            tap(expense => this.model = expense)
+        );
+        concat(persons$, categories$, expense$).subscribe({
+            error: (error) => { this.error = 'Failed to load data.'; }
+        });
+    }
 
+    isValid(): boolean {
+        return false;
+    }
+
+    submit(): void {
+        if (this.model) {
+            this.error = null;
+            this.expenseService.save(this.model).subscribe(
+                () => { this.dialogRef.close(true); },
+                (e) => { this.error = 'Failed to store data.'; }
+            );
+        }
+    }
+
+    cancel(): void {
+        this.dialogRef.close(false);
+    }
+
+    private loadExpense(): Observable<Expense> {
+        return iif(() => this.id !== null,
+            this.expenseService.getById(this.id as number),
+            RxUtils.asObservable(() => new Expense())
+        );
+    }
 }
