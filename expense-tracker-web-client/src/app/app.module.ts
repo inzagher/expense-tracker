b62843061d412c53f9b1e9fe3f34190200c5b2da
inzagher <IvanovDV87@yandex.ru>
2021-11-28 00:34:44 +0300

    Create core and shared modules

--- expense-tracker-web-client/src/app/app.module.ts
+++ expense-tracker-web-client/src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app.routing.module';
+import { CoreModule } from '@core/core.module';
+import { AppComponent } from './app.component';
 
 @NgModule({
     declarations: [
         AppComponent
     ],
     imports: [
+        CoreModule,
         BrowserModule,
-        BrowserAnimationsModule
+        BrowserAnimationsModule,
+        AppRoutingModule
     ],
     providers: [],
     bootstrap: [AppComponent]
