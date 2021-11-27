7cec3c81bd306783a1fae18190a005c8786bbead
inzagher <IvanovDV87@yandex.ru>
2021-11-28 00:53:51 +0300

    Fix app-routing file name

--- expense-tracker-web-client/src/app/app.module.ts
+++ expense-tracker-web-client/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppRoutingModule } from './app.routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from '@core/core.module';
 import { AppComponent } from './app.component';
 
