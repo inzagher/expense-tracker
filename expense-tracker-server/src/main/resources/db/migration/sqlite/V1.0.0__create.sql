d0cf38ef6c31fd9eb64be5d597caa644ae93f968
inzagher <IvanovDV87@yandex.ru>
2021-12-29 17:27:42 +0300

    Add expense table indexes

--- expense-tracker-server/src/main/resources/db/migration/sqlite/V1.0.0__create.sql
+++ expense-tracker-server/src/main/resources/db/migration/sqlite/V1.0.0__create.sql
@@ -2,3 +2,5 @@ create table backups (id integer not null, categories integer, expenses integer,
 create table persons (id integer not null, name varchar(255), primary key (id));
 create table categories (id integer not null, name varchar(255), color_red integer, color_green integer, color_blue integer, description varchar(255), obsolete boolean, primary key (id));
 create table expenses (id integer not null, date datetime, amount numeric(19,2), category_id integer, person_id integer, description varchar(255), primary key (id));
+create index expenses_date_idx on expenses(date);
+create index expenses_description_idx on expenses(description);
