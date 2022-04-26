1b8720043c631b44683c471cf3dc76c741ba721c
inzagher <IvanovDV87@yandex,ru>
2022-04-26 20:42:53 +0300

    Fix some SonarLint issues in ExpenseSpecification.

--- expense-tracker-server/src/main/java/inzagher/expense/tracker/server/model/specification/ExpenseSpecification.java
+++ expense-tracker-server/src/main/java/inzagher/expense/tracker/server/model/specification/ExpenseSpecification.java
@@ -2,6 +2,7 @@ package inzagher.expense.tracker.server.model.specification;
 
 import inzagher.expense.tracker.server.model.criteria.ExpenseSearchCriteria;
 import inzagher.expense.tracker.server.model.entity.ExpenseEntity;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.domain.Specification;
 
@@ -16,39 +17,41 @@ public class ExpenseSpecification implements Specification<ExpenseEntity> {
     private final ExpenseSearchCriteria criteria;
 
     @Override
-    public Predicate toPredicate(Root<ExpenseEntity> expense, CriteriaQuery<?> query, CriteriaBuilder cb) {
+    public Predicate toPredicate(@NonNull Root<ExpenseEntity> expense,
+                                 @NonNull CriteriaQuery<?> criteria,
+                                 @NonNull CriteriaBuilder builder) {
         var predicates = new ArrayList<Predicate>();
-        if (criteria.getCategoryIdentifiers() != null && !criteria.getCategoryIdentifiers().isEmpty()) {
-            var in = cb.in(expense.get("category").get("id"));
-            criteria.getCategoryIdentifiers().forEach(in::value);
+        if (this.criteria.getCategoryIdentifiers() != null && !this.criteria.getCategoryIdentifiers().isEmpty()) {
+            var in = builder.in(expense.get("category").get("id"));
+            this.criteria.getCategoryIdentifiers().forEach(in::value);
             predicates.add(in);
         }
-        if (criteria.getPersonIdentifiers() != null && !criteria.getPersonIdentifiers().isEmpty()) {
-            var in = cb.in(expense.get("person").get("id"));
-            criteria.getPersonIdentifiers().forEach(in::value);
+        if (this.criteria.getPersonIdentifiers() != null && !this.criteria.getPersonIdentifiers().isEmpty()) {
+            var in = builder.in(expense.get("person").get("id"));
+            this.criteria.getPersonIdentifiers().forEach(in::value);
             predicates.add(in);
         }
-        if (criteria.getDateExact() != null) {
-            predicates.add(cb.equal(expense.get("date"), criteria.getDateExact()));
+        if (this.criteria.getDateExact() != null) {
+            predicates.add(builder.equal(expense.get("date"), this.criteria.getDateExact()));
         }
-        if (criteria.getDateFrom() != null) {
-            predicates.add(cb.greaterThanOrEqualTo(expense.get("date"), criteria.getDateFrom()));
+        if (this.criteria.getDateFrom() != null) {
+            predicates.add(builder.greaterThanOrEqualTo(expense.get("date"), this.criteria.getDateFrom()));
         }
-        if (criteria.getDateTo() != null) {
-            predicates.add(cb.lessThanOrEqualTo(expense.get("date"), criteria.getDateTo()));
+        if (this.criteria.getDateTo() != null) {
+            predicates.add(builder.lessThanOrEqualTo(expense.get("date"), this.criteria.getDateTo()));
         }
-        if (criteria.getAmountExact() != null) {
-            predicates.add(cb.equal(expense.get("amount"), criteria.getAmountExact()));
+        if (this.criteria.getAmountExact() != null) {
+            predicates.add(builder.equal(expense.get("amount"), this.criteria.getAmountExact()));
         }
-        if (criteria.getAmountFrom() != null) {
-            predicates.add(cb.greaterThanOrEqualTo(expense.get("amount"), criteria.getAmountFrom()));
+        if (this.criteria.getAmountFrom() != null) {
+            predicates.add(builder.greaterThanOrEqualTo(expense.get("amount"), this.criteria.getAmountFrom()));
         }
-        if (criteria.getAmountTo() != null) {
-            predicates.add(cb.lessThanOrEqualTo(expense.get("amount"), criteria.getAmountTo()));
+        if (this.criteria.getAmountTo() != null) {
+            predicates.add(builder.lessThanOrEqualTo(expense.get("amount"), this.criteria.getAmountTo()));
         }
-        if (criteria.getDescriptionLike() != null) {
-            predicates.add(cb.equal(expense.get("description"), criteria.getDescriptionLike()));
+        if (this.criteria.getDescriptionLike() != null) {
+            predicates.add(builder.equal(expense.get("description"), this.criteria.getDescriptionLike()));
         }
-        return cb.and(predicates.toArray(Predicate[]::new));
+        return builder.and(predicates.toArray(Predicate[]::new));
     }
 }
