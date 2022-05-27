eb31e10da73022bc02d30402342afb6dda143318
inzagher <IvanovDV87@yandex.ru>
2022-05-27 23:40:20 +0300

    Enable second and third caching level(Hibernate).

--- expense-tracker-server/src/main/java/inzagher/expense/tracker/server/model/entity/PersonEntity.java
+++ expense-tracker-server/src/main/java/inzagher/expense/tracker/server/model/entity/PersonEntity.java
@@ -3,15 +3,19 @@ package inzagher.expense.tracker.server.model.entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
 
 import javax.persistence.*;
 import java.io.Serializable;
 
 @Getter
 @Setter
-@NoArgsConstructor
 @Entity
+@Cacheable
+@NoArgsConstructor
 @Table(name = "persons")
+@Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
 public class PersonEntity implements Serializable {
 
     @Id
--- expense-tracker-server/src/main/java/inzagher/expense/tracker/server/repository/CategoryRepository.java
+++ expense-tracker-server/src/main/java/inzagher/expense/tracker/server/repository/CategoryRepository.java
@@ -1,9 +1,17 @@
 package inzagher.expense.tracker.server.repository;
 
 import inzagher.expense.tracker.server.model.entity.CategoryEntity;
+import lombok.NonNull;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CategoryRepository extends JpaRepository<CategoryEntity, Long> {
+    @NonNull
+    @Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE, region = "STATIC_DATA")
+    List<CategoryEntity> findAll();
 }
