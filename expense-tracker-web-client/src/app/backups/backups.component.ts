ef1a96cfa4fb5c8010b1a6659f6835ffcff5c194
inzagher <IvanovDV87@yandex.ru>
2022-06-02 00:57:47 +0300

    Generate backup module components.

---
+++ expense-tracker-web-client/src/app/backups/backups.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'backups',
+    templateUrl: './backups.component.html',
+    styleUrls: ['./backups.component.scss']
+})
+export class BackupsComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit(): void {
+    }
+}
--- expense-tracker-web-client/src/app/backups/backups.module.ts
+++ expense-tracker-web-client/src/app/backups/backups.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from '@shared/shared.module';
 import { BackupsRoutingModule } from '@backups/backups-routing.module';
+import { BackupsComponent } from '@backups/backups.component';
 
 @NgModule({
-    declarations: [],
+    declarations: [
+        BackupsComponent
+    ],
     imports: [
         CommonModule,
+        SharedModule,
         BackupsRoutingModule
     ]
 })
