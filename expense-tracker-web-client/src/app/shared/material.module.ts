0dc08662cddd3f60e451771c38434906ab9aceea
inzagher <IvanovDV87@yandex.ru>
2022-06-04 01:48:19 +0300

    Add ripple effect.

--- expense-tracker-web-client/src/app/shared/material.module.ts
+++ expense-tracker-web-client/src/app/shared/material.module.ts
@@ -2,17 +2,20 @@ import { NgModule } from "@angular/core";
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatRippleModule } from "@angular/material/core";
 
 @NgModule({
     imports: [
         MatToolbarModule,
         MatButtonModule,
-        MatIconModule
+        MatIconModule,
+        MatRippleModule
     ],
     exports: [
         MatToolbarModule,
         MatButtonModule,
-        MatIconModule
+        MatIconModule,
+        MatRippleModule
     ]
 })
 export class MaterialModule { }
