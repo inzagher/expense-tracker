4e214d3527afcf4f763780c682426ce3b25df808
inzagher <IvanovDV87@yandex.ru>
2022-06-07 00:03:54 +0300

    Add material form modules.

--- expense-tracker-web-client/src/app/shared/material.module.ts
+++ expense-tracker-web-client/src/app/shared/material.module.ts
@@ -2,20 +2,34 @@ import { NgModule } from "@angular/core";
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { MatRippleModule } from "@angular/material/core";
+import { MatRippleModule, MatNativeDateModule } from "@angular/material/core";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatSelectModule } from "@angular/material/select";
+import { MatDatepickerModule  } from "@angular/material/datepicker";
 
 @NgModule({
     imports: [
         MatToolbarModule,
         MatButtonModule,
         MatIconModule,
-        MatRippleModule
+        MatRippleModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatSelectModule,
+        MatDatepickerModule,
+        MatNativeDateModule
     ],
     exports: [
         MatToolbarModule,
         MatButtonModule,
         MatIconModule,
-        MatRippleModule
+        MatRippleModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatSelectModule,
+        MatDatepickerModule,
+        MatNativeDateModule
     ]
 })
 export class MaterialModule { }
