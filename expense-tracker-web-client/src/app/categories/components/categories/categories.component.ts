c2f3a66c1d70601ad005ef91d725360190d952d8
inzagher <IvanovDV87@yandex.ru>
2022-06-18 01:16:11 +0300

    Categories component functionality.

--- expense-tracker-web-client/src/app/categories/components/categories/categories.component.ts
+++ expense-tracker-web-client/src/app/categories/components/categories/categories.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { ChangeTitleCommand } from "@core/commands";
+import { CategoryDTO } from "@core/dto";
+import { Bus, CategoryService } from "@core/services";
+import { DialogService } from "@core/services/dialog.service";
+import { Observable, BehaviorSubject, switchMap } from "rxjs";
 
 @Component({
     selector: 'categories',
@@ -6,8 +12,42 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./categories.component.scss']
 })
 export class CategoriesComponent implements OnInit {
-    constructor() { }
+    public categories$: Observable<CategoryDTO[]> | null = null;
+    private update: BehaviorSubject<void> = new BehaviorSubject<void>(void 0);
+
+    constructor(private bus: Bus,
+                private router: Router,
+                private dialogService: DialogService,
+                private categoryService: CategoryService) { }
 
     ngOnInit(): void {
+        this.categories$ = this.update.pipe(switchMap(() => this.categoryService.findAll()));
+        this.bus.publish(new ChangeTitleCommand("Категории"));
+    }
+
+    ngOnDestroy(): void {
+        this.update.complete();
+    }
+
+    refresh(): void {
+        this.update.next();
+    }
+
+    add(): void {
+        this.router.navigate(['categories/editor'])
+    }
+
+    edit(category: CategoryDTO): void {
+        this.router.navigate(['categories/editor/' + category.id]);
+    }
+
+    delete(category: CategoryDTO): void {
+        let caption = 'Внимание!';
+        let question = 'Вы действительно хотите удалить категорию?';
+        let request$ = this.categoryService.deleteById(category.id as number);
+        this.dialogService.confirmAndExecute(caption, question, request$).subscribe({
+            next: () => { this.refresh(); },
+            error: (error) => { console.log(error); }
+        });
     }
 }
