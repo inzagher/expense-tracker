d6eb4f871f1164a7fcd66fe048cab0e970dac35d
inzagher <IvanovDV87@yandex.ru>
2022-06-17 23:17:41 +0300

    Fix expense search params.

--- expense-tracker-server/src/main/java/inzagher/expense/tracker/server/controller/ExpenseApiController.java
+++ expense-tracker-server/src/main/java/inzagher/expense/tracker/server/controller/ExpenseApiController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -22,24 +23,24 @@ public class ExpenseApiController {
     @GetMapping(path = "/api/expenses")
     @Operation(summary = "Find expenses")
     public List<ExpenseDTO> find(
-            @RequestParam(value = "persons", required = false) List<Long> persons,
-            @RequestParam(value = "categories", required = false) List<Long> categories,
+            @RequestParam(value = "persons[]", required = false) Long[] persons,
+            @RequestParam(value = "categories[]", required = false) Long[] categories,
             @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
-            @RequestParam(value = "date_exact", required = false) LocalDate dateExact,
+            @RequestParam(value = "dateExact", required = false) LocalDate dateExact,
             @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
-            @RequestParam(value = "date_from", required = false) LocalDate dateFrom,
+            @RequestParam(value = "dateFrom", required = false) LocalDate dateFrom,
             @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
-            @RequestParam(value = "date_to", required = false) LocalDate dateTo,
-            @RequestParam(value = "amount_exact", required = false) BigDecimal amountExact,
-            @RequestParam(value = "amount_from", required = false) BigDecimal amountFrom,
-            @RequestParam(value = "amount_to", required = false) BigDecimal amountTo,
+            @RequestParam(value = "dateTo", required = false) LocalDate dateTo,
+            @RequestParam(value = "amountExact", required = false) BigDecimal amountExact,
+            @RequestParam(value = "amountFrom", required = false) BigDecimal amountFrom,
+            @RequestParam(value = "amountTo", required = false) BigDecimal amountTo,
             @RequestParam(value = "description", required = false) String description) {
         var criteria = new ExpenseSearchCriteria();
         if (persons != null) {
-            persons.forEach(criteria.getPersonIdentifiers()::add);
+            Arrays.asList(persons).forEach(criteria.getPersonIdentifiers()::add);
         }
         if (categories != null) {
-            categories.forEach(criteria.getCategoryIdentifiers()::add);
+            Arrays.asList(categories).forEach(criteria.getCategoryIdentifiers()::add);
         }
         criteria.setDateExact(dateExact);
         criteria.setDateFrom(dateFrom);
