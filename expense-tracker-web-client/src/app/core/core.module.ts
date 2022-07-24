4d527da393d00fb4b94ed7dc9100c21d9a3c5e19
inzagher <IvanovDV87@yandex.ru>
2022-07-25 02:00:07 +0300

    Change application locale.

--- expense-tracker-web-client/src/app/core/core.module.ts
+++ expense-tracker-web-client/src/app/core/core.module.ts
@@ -1,8 +1,11 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule, registerLocaleData } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { MaterialModule } from '@material/material.module';
 import { LOCALE_ID, NgModule, Optional, SkipSelf } from '@angular/core';
 import { ConfirmationDialogComponent } from '@core/dialogs';
+
+import localeAngularRu from '@angular/common/locales/ru';
+registerLocaleData(localeAngularRu);
 
 @NgModule({
     declarations: [
@@ -14,7 +17,7 @@ import { ConfirmationDialogComponent } from '@core/dialogs';
         HttpClientModule
     ],
     providers: [
-        { provide: LOCALE_ID, useValue: "en-US" }
+        { provide: LOCALE_ID, useValue: "ru-RU" }
     ]
 })
 export class CoreModule {
