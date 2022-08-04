a846ea1db560dede02a136359b323972b95d0023
inzagher <IvanovDV87@yandex.ru>
2022-08-04 23:51:27 +0300

    Add period selector in dashboard component.

--- expense-tracker-web-client/src/app/dashboard/dashboard.component.ts
+++ expense-tracker-web-client/src/app/dashboard/dashboard.component.ts
@@ -1,34 +1,51 @@
 import { formatDate } from '@angular/common';
 import { Component, Inject, LOCALE_ID, OnDestroy, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { FormControl, UntypedFormGroup, Validators } from '@angular/forms';
+import { MAT_DATE_FORMATS } from '@angular/material/core';
+import { MatDatepicker } from '@angular/material/datepicker';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ChangeTitleCommand } from '@core/commands';
 import { CategoryDTO, CategoryReportItemDTO, YearlyReportItemDTO } from '@core/dto';
 import { BackupRestoredEvent } from '@core/events';
 import { BusMessage, Bus, ReportService } from '@core/services';
 import { DateUtils, MathUtils } from '@core/utils';
-import { Subscription } from 'rxjs';
+import { DateFormattingUtils } from '@material/date-formatting.utils';
+import { merge, Subscription, tap, toArray } from 'rxjs';
+import { Moment } from 'moment';
 
 @Component({
     selector: 'dashboard',
     templateUrl: './dashboard.component.html',
-    styleUrls: ['./dashboard.component.scss']
+    styleUrls: ['./dashboard.component.scss'],
+    providers: [
+        { provide: MAT_DATE_FORMATS, useValue: DateFormattingUtils.getYearMonthFormatSettings() }
+    ]
 })
 export class DashboardComponent implements OnInit, OnDestroy {
-    public categoryReport: CategoryReportItemDTO[] | null = null;
-    public yearlyReport: YearlyReportItemDTO[] | null = null;
-
-    private today: Date = new Date;
-    private subscription: Subscription | null = null;
+    subscription: Subscription | null = null;
+    categoryReport: CategoryReportItemDTO[] | null = null;
+    yearlyReport: YearlyReportItemDTO[] | null = null;
+    period: FormControl<Moment | null> | null = null;
+    form: UntypedFormGroup = new UntypedFormGroup({});
+    loading: boolean = false;
 
     constructor(private bus: Bus,
                 private router: Router,
+                private route: ActivatedRoute,
                 private reportService: ReportService,
                 @Inject(LOCALE_ID) private locale: string) { }
 
     ngOnInit(): void {
+        let today = new Date();
+        let year = this.getIntParamFromRoute('year', today.getFullYear());
+        let month = this.getIntParamFromRoute('month', today.getMonth() + 1) - 1;
+        this.period = new FormControl<Moment>(DateUtils.createUTCMoment(year, month, 1));
+        this.period.addValidators(Validators.required);
+        this.form.addControl('period', this.period);
+
         this.subscription = this.bus.messages$.subscribe(m => this.onBusMessage(m));
         this.bus.publish(new ChangeTitleCommand("Учет расходов"));
-        this.reload();
+        this.reloadReportData();
     }
 
     ngOnDestroy(): void {
@@ -37,20 +54,31 @@ export class DashboardComponent implements OnInit, OnDestroy {
         }
     }
 
+    onPeriodChanged(selected: Moment, picker: MatDatepicker<Moment>): void {
+        let value = this.period?.value!;
+        value.year(selected.year());
+        value.month(selected.month());
+        value.date(1);
+        picker.close();
+        this.period?.setValue(value);
+        this.reloadReportData();
+    }
+
     onCategorySelected(category: CategoryDTO | null): void {
-        if (category) {
+        if (category && this.period?.valid) {
             let categories = [ category.id ];
-            let periodStart = this.toLocalDate(DateUtils.createUTCDate(this.today.getFullYear(), this.today.getMonth(), 1));
-            let periodEnd = this.toLocalDate(DateUtils.createUTCDate(this.today.getFullYear(), this.today.getMonth() + 1, 0));
+            let selectedPeriodDate = DateUtils.toUtcDateFromMoment(this.period.value as Moment);
+            let periodStart = this.toLocalDate(DateUtils.createUTCDate(selectedPeriodDate.getFullYear(), selectedPeriodDate.getMonth(), 1));
+            let periodEnd = this.toLocalDate(DateUtils.createUTCDate(selectedPeriodDate.getFullYear(), selectedPeriodDate.getMonth() + 1, 0));
             let queryParams = { categories: categories, dateFrom: periodStart, dateTo: periodEnd };
             this.router.navigate(['expenses/list/search'], { queryParams: queryParams });
         }
     }
 
     onMonthSelected(month: number | null): void {
-        if (typeof(month) === 'number') {
-            let year = this.today.getFullYear();
-            this.router.navigate([`expenses/list/monthly/${year}/${month}`]);
+        if (typeof(month) === 'number' && this.period?.valid) {
+            let selectedPeriodDate = DateUtils.toUtcDateFromMoment(this.period.value as Moment);
+            this.router.navigate([`expenses/list/monthly/${selectedPeriodDate.getFullYear()}/${month}`]);
         }
     }
 
@@ -64,19 +92,28 @@ export class DashboardComponent implements OnInit, OnDestroy {
 
     private onBusMessage(message: BusMessage) {
         if (message instanceof BackupRestoredEvent) {
-            this.reload();
+            this.reloadReportData();
         }
     }
 
-    private reload(): void {
-        let year = this.today.getFullYear();
-        let month = this.today.getMonth() + 1;
-        this.reportService.getCategoryReport(year, month).subscribe(
-            (items) => this.categoryReport = items.filter(this.isCategoryReportItemVisible)
-        );
-        this.reportService.getYearlyReport(year).subscribe(
-            (items) => this.yearlyReport = items
-        );
+    private reloadReportData(): void {
+        if (this.period?.valid) {
+            this.loading = true;
+            this.form.get('period')?.disable();
+            let selectedPeriodDate = DateUtils.toUtcDateFromMoment(this.period.value as Moment);
+            let year = selectedPeriodDate.getFullYear();
+            let month = selectedPeriodDate.getMonth() + 1;
+            let yearlyReport$ = this.reportService.getCategoryReport(year, month).pipe(
+                tap((items) => this.categoryReport = items.filter(this.isCategoryReportItemVisible))
+            );
+            let categoryReport$ = this.reportService.getYearlyReport(year).pipe(
+                tap((items) => this.yearlyReport = items)
+            );
+            merge(yearlyReport$, categoryReport$).pipe(toArray()).subscribe({
+                next: () => { this.form.get('period')?.enable(); this.loading = false; },
+                error: (e) => { console.error(e); this.loading = false; }
+            });
+        }
     }
 
     private isCategoryReportItemVisible(item: CategoryReportItemDTO): boolean {
@@ -85,5 +122,10 @@ export class DashboardComponent implements OnInit, OnDestroy {
 
     private toLocalDate(date: Date): string {
         return formatDate(date, 'yyyy-MM-dd', this.locale);
+    }
+
+    private getIntParamFromRoute(paramName: string, orElse: number): number {
+        let param  = this.route.snapshot.paramMap.get(paramName);
+        return Number.parseInt(param ?? orElse.toString());
     }
 }
--- expense-tracker-web-client/src/app/dashboard/dashboard.module.ts
+++ expense-tracker-web-client/src/app/dashboard/dashboard.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '@shared/shared.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DashboardRoutingModule } from '@dashboard/dashboard-routing.module';
 import { DashboardComponent } from '@dashboard/dashboard.component';
 
@@ -11,6 +12,8 @@ import { DashboardComponent } from '@dashboard/dashboard.component';
     imports: [
         CommonModule,
         SharedModule,
+        FormsModule,
+        ReactiveFormsModule,
         DashboardRoutingModule
     ]
 })
