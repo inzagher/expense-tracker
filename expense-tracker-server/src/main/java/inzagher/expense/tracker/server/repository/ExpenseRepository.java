eab569502990bd2157111e1c484ae6b9f13ce189
inzagher <IvanovDV87@yandex.ru>
2023-07-16 01:04:08 +0300

    Fix N+1 query problem.

--- expense-tracker-server/src/main/java/inzagher/expense/tracker/server/repository/ExpenseRepository.java
+++ expense-tracker-server/src/main/java/inzagher/expense/tracker/server/repository/ExpenseRepository.java
@@ -1,6 +1,11 @@
 package inzagher.expense.tracker.server.repository;
 
 import inzagher.expense.tracker.server.model.entity.ExpenseEntity;
+import lombok.NonNull;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
@@ -8,11 +13,23 @@ import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ExpenseRepository extends
         JpaRepository<ExpenseEntity, Long>,
         JpaSpecificationExecutor<ExpenseEntity> {
+
+    @NonNull
+    @Override
+    @EntityGraph(attributePaths = { "category", "person" })
+    Optional<ExpenseEntity> findById(@NonNull Long id);
+
+    @NonNull
+    @Override
+    @EntityGraph(attributePaths = { "category", "person" })
+    Page<ExpenseEntity> findAll(@NonNull Specification<ExpenseEntity> spec,
+                                @NonNull Pageable pageable);
 
     @Query("SELECT c.id, SUM(e.amount) " +
            "FROM ExpenseEntity e RIGHT JOIN e.category c " +
